49eef060ca876d2c5ed3425c5ca52573558568dd
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-14 02:16:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution here
-let heyJude =  "Na na na na na na na, na na na na, hey Jude.";
-
-heyJude.repeat(16);
+let heyJude =  function(){
+  "Na na na na na na na, na na na na, hey Jude.".repeat(16);
+};
+heyJude();
