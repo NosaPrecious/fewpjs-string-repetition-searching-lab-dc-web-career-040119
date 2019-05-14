9447782bc50db80bf19b45977912c13c6b657029
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-14 02:20:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Code your solution here
-let heyJude =  function(){
-  let str = "Na na na na na na na, na na na na, hey Jude.";
-  str.repeat(16);
+// let heyJude =  function(){
+//   let str = "Na na na na na na na, na na na na, hey Jude.";
+//   str.repeat(16);
+// };
+// heyJude();
+
+const heyJude = function(str, num){
+  return (new Array(num + 1)).join(str);
 };
-heyJude();
+
+heyJude("Na na na na na na na, na na na na, hey Jude.", 16);
